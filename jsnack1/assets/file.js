0f02291ef409c9a4053f98d0604bf3d54b46f22c
ndongogueye <bayeg2003@gmail.com>
2023-07-06 16:52:34 +0200

    console log

--- jsnack1/assets/file.js
+++ jsnack1/assets/file.js
@@ -1,2 +1,11 @@
 let nomi = ["Mario", "Luigi", "Giovanni", "Francesca", "Maria", "Antonio"];
 let cognomi = ["Rossi", "Bianchi", "Verdi", "Russo", "Ferrari", "Esposito"];
+
+
+
+    let nomeCasuale = [Math.floor(Math.random() * nomi.lenght)];
+    let cognomeCasuale = [Math.floor(Math.random() * cognomi.lenght)];
+
+
+    console.log(nomeCasuale + cognomeCasuale)
+
