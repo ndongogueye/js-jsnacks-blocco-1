72b0b8e1b90f2bea4dc6bf776de3f9d4a915437f
ndongogueye <bayeg2003@gmail.com>
2023-07-07 10:25:12 +0200

    col ciclo

--- jsnack1/assets/file.js
+++ jsnack1/assets/file.js
@@ -2,10 +2,14 @@ let nomi = ["Mario", "Luigi", "Giovanni", "Francesca", "Maria", "Antonio"];
 let cognomi = ["Rossi", "Bianchi", "Verdi", "Russo", "Ferrari", "Esposito"];
 
 
-
+for (let i = 0; i < 30; i++) {
     let nomeCasuale = nomi [Math.floor(Math.random() * nomi.length)];
     let cognomeCasuale = cognomi [Math.floor(Math.random() * cognomi.length)];
 
-
     console.log(nomeCasuale + cognomeCasuale)
+}
+   
+
+
+  
 
